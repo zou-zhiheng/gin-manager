49978bc99e0abf09934c2032ec31b534a4ea4a35
sttch <421786612@qq.com>
2023-08-05 03:10:25 +0800

    start

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func init() {
 }
 
 func main() {
-	fmt.Println("rabbitmq coding")
+	fmt.Println("rabbitmq start")
 	engine := router.GetEngine()
 	if err := engine.Run(":8060"); err != nil {
 		panic(err)
